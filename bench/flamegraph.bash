87309a3424772c86312c01f74fe494487586bf02
theodus <theodusbutler@gmail.com>
2019-02-03 15:28:24 -0500

    cleanup after flamegraph

--- bench/flamegraph.bash
+++ bench/flamegraph.bash
@@ -7,6 +7,6 @@ make THREADS=4 MEMORY=1024 N_TOTAL=100000
 sudo LD_PRELOAD=../build/libsnmallocshim.so perf record -g ./bench
 sudo perf script > out.perf
 ~/src/FlameGraph/stackcollapse-perf.pl out.perf > out.folded
-~/src/FlameGraph/flamegraph.pl \
-  --width 2400 \
-  out.folded > out.svg
+~/src/FlameGraph/flamegraph.pl out.folded > flame.svg
+
+rm -f out.* perf.*
